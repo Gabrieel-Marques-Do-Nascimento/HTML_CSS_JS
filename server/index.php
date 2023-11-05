0c306bed00e76354d9c402a126a71f53eafcdb28
Gabriel marques do nascimento <142952581+Gabrieel-Marques-Do-Nascimento@users.noreply.github.com>
2023-11-05 09:53:02 -0300

    Create index.php

---
+++ server/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <p>
+            <label for="email">Email</label>
+            <input type="text" name="" id="email">
+        </p>
+<p>
+    <label for="senha">Senha</label>
+    <input type="password" name="" id="senha">
+</p>
+<button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
